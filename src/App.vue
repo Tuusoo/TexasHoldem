0a68bc85459318632ea66bf973213ece2c9c3b8d
Tuusoo <suhao1998@yeah.net>
2024-03-01 17:36:13 +0800

    feat:心跳检测逻辑

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,23 @@ onMounted(() => {
     (document.getElementById("app") as HTMLElement).style.height =
         document.documentElement.clientHeight + "px";
 });
+
+onMounted(() => {
+    let ws = new WebSocket("ws://localhost:3000/login");
+    const pin = new Date().getTime();
+    sessionStorage.setItem("userId", pin.toString());
+    ws.onopen = () => {
+        ws.send(`user:${pin}`);
+    };
+    // setInterval(() => {
+    //     ws.send(`heartBeat:${pin}`);
+    // }, 2000);
+    ws.onmessage = e => {
+        if (e.data.indexOf("heartBeat:") > -1) {
+            ws.send(`online:${pin}:${new Date().getTime()}`);
+        }
+    };
+});
 </script>
 
 <template>
