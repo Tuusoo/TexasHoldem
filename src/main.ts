6075c31ad50345b60777e85e2d2eedfb092b106f
Tuusoo <suhao1998@yeah.net>
2024-03-01 23:52:34 +0800

    feat:vconsole

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import "./style.less";
 import App from "./App.vue";
 import router from "./router";
 import "vant/lib/icon/style/index"
+import VConsole from "vconsole";
+
+const vconsole = new VConsole();
 
 createApp(App).use(router).mount("#app");
